330a111ad6e06724c77e93ebd3862ce1248d3ba1
americo chacaltana <achacaltana1987@gmail.com>
2012-07-27 06:05:52 -0500

    GRupoDeEstudio - Americo

--- CRM_IST_BenedictoXVI/test/upc/poo/GrupoDeEstudioTest.java
+++ CRM_IST_BenedictoXVI/test/upc/poo/GrupoDeEstudioTest.java
@@ -1,13 +1,24 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package upc.poo;
 
-/**
- *
- * @author chasky
- */
+import junit.framework.Assert;
+import org.junit.Test;
+
 public class GrupoDeEstudioTest {
+
+    @Test
+    public void DebeExistirLaClaseVentaYNoSerNull1 () {
+        GrupoDeEstudio grupoestudio = new GrupoDeEstudio();
+        Assert.assertNotNull(grupoestudio); 
+    }
     
+    /*
+    @Test
+    public void DebeExistirLaClaseVentaYNoSerNull2 () {
+    //String concepto, String tipoDocumento, String numero, String empresa, String fechaEmision, String fechaVencimiento, String fechaPago, String estado, double subtotal, String moneda, String observaciones
+        Venta venta = new Venta("Java SE", "Factura", "001-001", "ABS Soluciones", "11-06-2009", "11-07-2009", "30-06-2009", "cancelada", 200.00, "dolares", "...");
+        Assert.assertNotNull(venta); 
+    }
+    * 
+    */
+            
 }
