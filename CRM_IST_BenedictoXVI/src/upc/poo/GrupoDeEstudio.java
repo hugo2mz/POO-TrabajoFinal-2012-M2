ad796eac873791136f101cb1d7e86e135c7fbd7f
americo chacaltana <achacaltana1987@gmail.com>
2012-07-27 06:32:44 -0500

    GRupoDeEstudio - Americo

--- CRM_IST_BenedictoXVI/src/upc/poo/GrupoDeEstudio.java
+++ CRM_IST_BenedictoXVI/src/upc/poo/GrupoDeEstudio.java
@@ -1,5 +1,7 @@
 package upc.poo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 class GrupoDeEstudio {
@@ -21,30 +23,40 @@ class GrupoDeEstudio {
     }
 
     //Constructor para búsquedas
-    public GrupoDeEstudio(String nombre, String academia, String curso, Date fechaInicio, Date fechaFin, String estado) {
+    public GrupoDeEstudio(String nombre, String academia, String curso, String fechaInicio, String fechaFin, String estado) {
         this.nombre = nombre;
         this.academia = academia;
         this.curso = curso;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
         this.estado = estado;
+        
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            this.fechaInicio = formatoDelTexto.parse(fechaInicio);
+            this.fechaFin = formatoDelTexto.parse(fechaFin);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
     }
 
     //Constructor Mínimo para dar de alta
-    public GrupoDeEstudio(String nombre, Date fechaInicio, Date fechaFin) {
+    public GrupoDeEstudio(String nombre, String fechaInicio, String fechaFin) {
         this.nombre = nombre;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
+        
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            this.fechaInicio = formatoDelTexto.parse(fechaInicio);
+            this.fechaFin = formatoDelTexto.parse(fechaFin);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
     }
     
     //Construtor Completo para dar de alta
-    public GrupoDeEstudio(String nombre, String decripcion, String academia, String curso, Date fechaInicio, Date fechaFin, String estado, String instructures, String syllabus, String local, String aula, String mapsLatitud, String mapsLongitud) {
+    public GrupoDeEstudio(String nombre, String decripcion, String academia, String curso, String fechaInicio, String fechaFin, String estado, String instructures, String syllabus, String local, String aula, String mapsLatitud, String mapsLongitud) {
         this.nombre = nombre;
         this.decripcion = decripcion;
         this.academia = academia;
         this.curso = curso;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
         this.estado = estado;
         this.instructures = instructures;
         this.syllabus = syllabus;
@@ -52,12 +64,78 @@ class GrupoDeEstudio {
         this.aula = aula;
         this.mapsLatitud = mapsLatitud;
         this.mapsLongitud = mapsLongitud;
+        
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            this.fechaInicio = formatoDelTexto.parse(fechaInicio);
+            this.fechaFin = formatoDelTexto.parse(fechaFin);
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String getAcademia() {
+        return academia;
+    }
+
+    public String getAula() {
+        return aula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getDecripcion() {
+        return decripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public String getInstructures() {
+        return instructures;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public String getMapsLatitud() {
+        return mapsLatitud;
+    }
+
+    public String getMapsLongitud() {
+        return mapsLongitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSyllabus() {
+        return syllabus;
+    }
+
+    
+    public void setMapsLatitud(String mapsLatitud) {
+        this.mapsLatitud = mapsLatitud;
+    }
+
+    public void setMapsLongitud(String mapsLongitud) {
+        this.mapsLongitud = mapsLongitud;
     }
     
 
-    
-    
-    
     
     
 }
--- CRM_IST_BenedictoXVI/test/upc/poo/GrupoDeEstudioTest.java
+++ CRM_IST_BenedictoXVI/test/upc/poo/GrupoDeEstudioTest.java
@@ -6,19 +6,15 @@ import org.junit.Test;
 public class GrupoDeEstudioTest {
 
     @Test
-    public void DebeExistirLaClaseVentaYNoSerNull1 () {
+    public void DebeExistirLaClaseGrupoDeEstudioYNoSerNull1 () {
         GrupoDeEstudio grupoestudio = new GrupoDeEstudio();
         Assert.assertNotNull(grupoestudio); 
     }
     
-    /*
     @Test
-    public void DebeExistirLaClaseVentaYNoSerNull2 () {
-    //String concepto, String tipoDocumento, String numero, String empresa, String fechaEmision, String fechaVencimiento, String fechaPago, String estado, double subtotal, String moneda, String observaciones
-        Venta venta = new Venta("Java SE", "Factura", "001-001", "ABS Soluciones", "11-06-2009", "11-07-2009", "30-06-2009", "cancelada", 200.00, "dolares", "...");
-        Assert.assertNotNull(venta); 
+    public void DebeExistirLaClaseGrupoDeEstudioYNoSerNull2 () {    
+        GrupoDeEstudio grupoestudio = new GrupoDeEstudio("Grupo Telefonica Centrales", "12-07-2012", "22-07-2012");
+        Assert.assertNotNull(grupoestudio); 
     }
-    * 
-    */
             
 }
